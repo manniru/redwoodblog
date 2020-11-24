0acb6ae7adbc2e496d6bf00a3771c2b4a63361b2
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2020-11-24 09:04:23 +0100

    Initialised Source Files

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,40 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+
+  FieldError,
+} from '@redwoodjs/forms'
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <BlogLayout>
+      <Form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <TextField name="name" validation={{ required: true }} />
+
+        <FieldError name="name" />
+
+        <label htmlFor="email">Email</label>
+        <TextField name="email" validation={{ required: true }} />
+
+        <FieldError name="email" />
+
+        <label htmlFor="message">Message</label>
+        <TextAreaField name="message" validation={{ required: true }} />
+
+        <FieldError name="message" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
